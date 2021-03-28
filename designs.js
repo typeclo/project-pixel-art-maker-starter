162d7e8a34d9fc24cc70d09630b8628fa36ad9d7
Clouie Tantuan Gonzalez <clouie.tantuan@hotmail.com>
2021-03-28 03:07:41 +0200

    completing project

--- designs.js
+++ designs.js
@@ -3,8 +3,30 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+const sizePicker = document.getElementById("sizePicker");
+const pixelCanvas = document.getElementById("pixelCanvas");
+const colorPicker = document.getElementById("colorPicker")
 
-// Your code goes here!
+sizePicker.onsubmit = makeGrid;
 
+function makeGrid(event) {
+  event.preventDefault();
+  pixelCanvas.innerHTML = "";
+
+  const height = event.target.inputHeight.value;
+  const width = event.target.inputWidth.value;
+
+  for (i = 0; i < height; i++) {
+    const row = document.createElement("tr");
+    for (j = 0; j < width; j++) {
+      const cell = document.createElement("td");
+      cell.onclick = setCellColor
+      row.appendChild(cell);
+    }
+    pixelCanvas.appendChild(row);
+  }
+}
+
+function setCellColor() {
+    this.style.backgroundColor = colorPicker.value
 }
